8ec4a4783cc033dfe33c4646fb6c84ce91c7d1a7
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-17 15:34:04 +0200

    Add Comments, Fix platform generation.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -12,17 +13,13 @@ public class LevelGenerator : MonoBehaviour
 	[SerializeField] private int platformsToSpawn = 10;                         // How many platforms will be spawned until the end platform is spawned.
 	[SerializeField] private Vector2 platformOffset = default;                  // How much offset there is between platform spawns. (platform width / 2)
 	[SerializeField] private Transform platformSpawnPoint = default;            // Where to spawn the (next) platform.
+	[SerializeField] private Transform platformParent = default;                // Reference to the platforms parent.
 	[SerializeField] private float distanceToDespawnPlatform = 30f;             // Distance between the Main Camera and the platforms to despawn.
 	[Space]
 	[SerializeField] private List<GameObject> platformsInScene = new List<GameObject>();    // List with all the Platforms that are in the scene.
 	#endregion
 
 	#region Monobehaviour Callbacks
-	private void Start()
-	{
-		SpawnPlatforms();
-	}
-
 	private void Update()
 	{
 		for(int i = 0; i < platformsInScene.Count; i++)
@@ -36,14 +33,19 @@ public class LevelGenerator : MonoBehaviour
 	#endregion
 
 	#region Functions
-	private void SpawnPlatforms()
+	/// <summary>
+	/// Generates all the platforms needed. This is done via an enumerator because the Game Manager will have to sequence which tasks will be performed.
+	/// </summary>
+	/// <returns></returns>
+	public IEnumerator Generate()
 	{
 		for(int i = 0; i < platformsToSpawn; i++)
 		{
 			Vector2 platformSpawnPos = new Vector2(platformSpawnPoint.position.x + platformOffset.x * i, platformOffset.y);
-			GameObject newPlatformGO = Instantiate(platformPrefabs[Random.Range(0, platformPrefabs.Length)], platformSpawnPos, Quaternion.identity);
+			GameObject newPlatformGO = Instantiate(platformPrefabs[Random.Range(0, platformPrefabs.Length)], platformSpawnPos, Quaternion.identity, platformParent);
 			platformsInScene.Add(newPlatformGO);
 		}
+		yield return new WaitForEndOfFrame();
 	}
 
 	/// <summary>
