88e8f65e3517e6dcbd49ca08933d89646f706847
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-17 19:58:37 +0200

    Add SmoothCam.cs

---
+++ Assets/Scripts/SmoothCam.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class SmoothCam : MonoBehaviour
+{
+	#region Variables
+	[SerializeField] private Transform target = default;            // what to follow.
+	[SerializeField] private float smoothing = default;             // How "Smooooth" the camera follows the target.
+	[SerializeField] private Vector3 offset = default;              // Offset from the target transform.
+
+	private Vector3 desiredPos;
+	private Vector3 smoothedPos;
+	#endregion
+
+	#region Properties
+	public Transform Target { get => target; set => target = value; }
+	#endregion
+
+	#region Monobehaviour Callbacks
+	private void Update()
+	{
+	}
+
+	private void FixedUpdate()
+	{
+		desiredPos = new Vector3(offset.x + target.position.x, offset.y, -10);
+		smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothing * Time.deltaTime);
+		transform.position = smoothedPos;
+	}
+	#endregion
+}
