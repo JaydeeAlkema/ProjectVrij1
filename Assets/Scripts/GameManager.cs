f33c35c3fab4026c8601526e7791d5f8d221822c
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-17 15:33:16 +0200

    Add Game Manager.
    This manages the main game scene.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Manages the main scene.
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+	#region Variables
+	[SerializeField] LevelGenerator levelGenerator = default;           // Reference to the level generator in the scene.
+	#endregion
+
+	#region Monobehaviour Callbacks
+	private void Start()
+	{
+		StartCoroutine(BeginGame());
+	}
+	#endregion
+
+	#region Functions
+	private IEnumerator BeginGame()
+	{
+		yield return StartCoroutine(levelGenerator.Generate());
+	}
+	#endregion
+}
