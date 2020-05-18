0cc2cc42f72c080be4e51d46504021a4086db6a1
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-18 14:52:51 +0200

    Add full clamping support

--- Assets/Scripts/SmoothCam.cs
+++ Assets/Scripts/SmoothCam.cs
@@ -8,6 +8,7 @@ public class SmoothCam : MonoBehaviour
 	[SerializeField] private Vector3 offset = default;              // Offset from the target transform.
 	[SerializeField] private Vector2 minClamp = default;            // Minimum clamp of the Camera (How far left it can go)
 	[SerializeField] private Vector2 maxClamp = default;            // Naximum clamp of the Camera (How far Right it can go)
+	[SerializeField] private bool clamping = true;                  // is clamping enabled.
 
 	private Vector3 desiredPos;
 	private Vector3 smoothedPos;
@@ -20,14 +21,18 @@ public class SmoothCam : MonoBehaviour
 	#region Monobehaviour Callbacks
 	private void Update()
 	{
-		Vector3 pos = transform.position;
-		pos.x = Mathf.Clamp(pos.x, minClamp.x, maxClamp.x);
-		transform.position = pos;
+		if(clamping)
+		{
+			Vector3 pos = transform.position;
+			pos.x = Mathf.Clamp(pos.x, minClamp.x, maxClamp.x);
+			pos.y = Mathf.Clamp(pos.y, minClamp.y, maxClamp.y);
+			transform.position = pos;
+		}
 	}
 
 	private void FixedUpdate()
 	{
-		desiredPos = new Vector3(offset.x + target.position.x, offset.y, -10);
+		desiredPos = new Vector3(offset.x + target.position.x, offset.y + target.position.y, offset.z);
 		smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothing * Time.deltaTime);
 		transform.position = smoothedPos;
 	}
