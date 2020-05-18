61d712d0ba398844a0fee13115cb52bcaf29777f
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-18 13:25:23 +0200

    Add QuickTimeEvent.cs

---
+++ Assets/Scripts/QuickTimeEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class QuickTimeEvent : MonoBehaviour
+{
+	#region Variables
+	[SerializeField] private KeyCode[] keysToPress = default;               // Which keys to press for this Quick Time Event.
+	#endregion
+
+	#region Functions
+	private void GetUserInput()
+	{
+
+	}
+	#endregion
+}
