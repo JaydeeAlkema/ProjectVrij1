c75e0758ac4ab87a43d4138760bb9af19a5117c4
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-20 21:14:20 +0200

    Add Simple class that return when target has entered collision

---
+++ Assets/Scripts/PlayerLightArea.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerLightArea : MonoBehaviour
+{
+	#region Variables
+	[SerializeField] private GameObject targetInLightArea = default;        // GameObject in the light area
+	#endregion
+
+	#region Properties
+	public GameObject TargetInLightArea { get => targetInLightArea; set => targetInLightArea = value; }
+	#endregion
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.gameObject.GetComponent<IDamageable>() != null)
+		{
+			targetInLightArea = collision.gameObject;
+			transform.GetComponentInParent<Player>().OnCollisionDetected(this);
+			Debug.Log("Target in Light Area!");
+		}
+	}
+}
