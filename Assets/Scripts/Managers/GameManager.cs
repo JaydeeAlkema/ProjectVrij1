96056484f3260089ecf1cf60b4dccdec064b374d
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-06-01 00:46:00 +0200

    Add Scene Fade and Audio Fade

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
 
 	[SerializeField] private GameObject cam = default;                  // Reference to the main camera in the scene. (I know Camera.main exists)
 	[Space]
-	[SerializeField] private SceneFader sceneFader = default;			// Reference to the scene fader.
+	[SerializeField] private SceneFader sceneFader = default;           // Reference to the scene fader.
 	[SerializeField] private GameObject playerPrefab = default;         // Reference to the Player Prefab.
 	[SerializeField] private Transform playerPrefabSpawnPos = default;  // Where the player will be spawned.
 	[Space]
@@ -52,6 +52,7 @@ public class GameManager : MonoBehaviour
 		cam.GetComponent<SmoothFollow>().Target = playerInstance.transform;
 		cam.SetActive(true);
 
+		AudioManager.Instance.FadeInBackgroundMusic();
 		sceneFader.Fade();
 		yield return new WaitForEndOfFrame();
 	}
