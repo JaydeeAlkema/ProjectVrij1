fbc7593e36480d9be79e43af0a92e0e7826ed368
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-06-04 18:05:35 +0200

    Add Enemy Spawn Manager

---
+++ Assets/Scripts/Managers/EnemySpawnerManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerManager : MonoBehaviour
+{
+	#region Variables
+	[SerializeField] private Transform[] spawnPositions = default;  // spawn Pos for the enemies that spawn to the right of the screen. 0 = left, 1 = right.
+	[SerializeField] private GameObject[] enemiesToSpawn = default; // Array with the enemies that can spawn.. 0 = left, 1 = right.
+	[Space]
+	[SerializeField] private float spawnInterval = 10f; // Time between enemy spawns.
+	[SerializeField] private List<GameObject> enemiesInScene = new List<GameObject>();  // List with all the enemies in the scene.
+
+	private int enemyIndex = 0;
+	private Transform followTransform = default;
+	#endregion
+
+	#region Monobehaviour Callbacks
+	private void Start()
+	{
+		StartCoroutine(SpawnEnemiesAtInterval());
+	}
+
+	private void Update()
+	{
+		if(followTransform == null) followTransform = GameManager.Instance.PlayerInstance.transform;
+		else transform.position = followTransform.position;
+	}
+	#endregion
+
+	#region Functions
+	private IEnumerator SpawnEnemiesAtInterval()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(spawnInterval);
+			int randInt = Random.Range(0, 2);
+			GameObject enemyGO = Instantiate(enemiesToSpawn[randInt], spawnPositions[randInt].position, Quaternion.identity);
+			enemiesInScene.Add(enemyGO);
+		}
+	}
+	#endregion
+}
