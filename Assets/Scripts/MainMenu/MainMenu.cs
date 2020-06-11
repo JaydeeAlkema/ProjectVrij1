4bd110f09924f8695ac6492287ec4fec77244508
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-06-11 15:23:25 +0200

    Add Main Menu class

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	#region Variables
+	[Header("Custom Buttons")]
+	[SerializeField] private CustomButton startButton = default;
+	[SerializeField] private CustomButton howToPlayButton = default;
+	[SerializeField] private CustomButton quitButton = default;
+
+	[Header("HowToPlay Screen")]
+	[SerializeField] private GameObject howToPlayObject = default;
+	[SerializeField] private CustomButton backToMainMenuButton = default;
+	#endregion
+
+	#region Monobehaviour
+	private void Start()
+	{
+		startButton.buttonClickDelegate += StartButton_OnClick;
+		howToPlayButton.buttonClickDelegate += HowToPlayButton_OnClick;
+		quitButton.buttonClickDelegate += QuitButton_OnClick;
+		backToMainMenuButton.buttonClickDelegate += BackToMainMenuButton_OnClick;
+	}
+	#endregion
+
+	#region Functions
+	public void StartButton_OnClick()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void HowToPlayButton_OnClick()
+	{
+		howToPlayObject.SetActive(true);
+	}
+
+	public void BackToMainMenuButton_OnClick()
+	{
+		howToPlayObject.SetActive(false);
+	}
+
+	public void QuitButton_OnClick()
+	{
+		Application.Quit();
+	}
+	#endregion
+}
