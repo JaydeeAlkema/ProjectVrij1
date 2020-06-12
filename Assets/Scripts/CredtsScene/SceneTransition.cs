2b68814c641062ac9a72ab07a02f901aa7a59c84
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-06-12 22:41:49 +0200

    Add Simple transition script that can be called via animator

---
+++ Assets/Scripts/CredtsScene/SceneTransition.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+	public void LoadScene(int buildIndex)
+	{
+		SceneManager.LoadScene(buildIndex);
+	}
+}
