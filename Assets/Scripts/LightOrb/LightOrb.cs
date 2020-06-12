2a5e6457fc4a400594ea5c8b1c02abdc3cfd90a5
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-06-12 12:28:54 +0200

    Add Class

---
+++ Assets/Scripts/LightOrb/LightOrb.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightOrb : MonoBehaviour, ICollectable
+{
+	#region Variables
+	[SerializeField] private SpriteRenderer spriteRenderer = default;           // Reference to the Sprite Renderer component.
+	[SerializeField] private Light2D light2D = default;                         // Reference to the light 2d component.
+	[SerializeField] private CircleCollider2D circleCollider = default;         // Reference to the CircleCollider2D component.
+	[SerializeField] private GameObject onCollectParticlesPrefab = default;     // Particle system prefab that spawns when collected.
+	#endregion
+
+	#region ICollectable Methods
+	public void Collect()
+	{
+		spriteRenderer.enabled = false;
+		light2D.enabled = false;
+		circleCollider.enabled = false;
+		Instantiate(onCollectParticlesPrefab, transform.position, Quaternion.identity);
+		Destroy(gameObject, 2f);
+	}
+	#endregion
+}
