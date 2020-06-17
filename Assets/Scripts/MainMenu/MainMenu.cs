5bd6d90802c2831f46a14df909346c8552b598a3
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-06-17 16:02:48 +0200

    Add functionality to the how to play scene buttons

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -11,28 +11,34 @@ public class MainMenu : MonoBehaviour
 
 	[Header("HowToPlay Screen")]
 	[SerializeField] private GameObject howToPlayObject = default;
+	[SerializeField] private GameObject[] howToPlayScreens = default;
 	[SerializeField] private CustomButton backToMainMenuButton = default;
+	[SerializeField] private CustomButton nextScreenButton = default;
+	[SerializeField] private CustomButton previousScreenButton = default;
+
+	int howToPlayScreenIndex = 0;
 	#endregion
 
 	#region Monobehaviour
 	private void Start()
 	{
+		// Main Menu
 		startButton.buttonClickDelegate += StartButton_OnClick;
 		howToPlayButton.buttonClickDelegate += HowToPlayButton_OnClick;
 		quitButton.buttonClickDelegate += QuitButton_OnClick;
+
+		// How to Play menu
 		backToMainMenuButton.buttonClickDelegate += BackToMainMenuButton_OnClick;
+		nextScreenButton.buttonClickDelegate += HowToPlayNextButton_OnClick;
+		previousScreenButton.buttonClickDelegate += HowToPlayPreviousButton_OnClick;
 	}
 	#endregion
 
 	#region Functions
+	// Main Menu functions
 	public void StartButton_OnClick()
 	{
 		SceneManager.LoadScene(1);
-	}
-
-	public void HowToPlayButton_OnClick()
-	{
-		howToPlayObject.SetActive(true);
 	}
 
 	public void BackToMainMenuButton_OnClick()
@@ -43,6 +49,30 @@ public class MainMenu : MonoBehaviour
 	public void QuitButton_OnClick()
 	{
 		Application.Quit();
+	}
+
+	// How to Play functions
+	public void HowToPlayButton_OnClick()
+	{
+		howToPlayObject.SetActive(true);
+	}
+
+	public void HowToPlayNextButton_OnClick()
+	{
+		if(howToPlayScreenIndex == 0) howToPlayScreenIndex = 1;
+		SetHowToPlayScreen();
+	}
+
+	public void HowToPlayPreviousButton_OnClick()
+	{
+		if(howToPlayScreenIndex == 1) howToPlayScreenIndex = 0;
+		SetHowToPlayScreen();
+	}
+
+	private void SetHowToPlayScreen()
+	{
+		for(int i = 0; i < howToPlayScreens.Length; i++) howToPlayScreens[i].SetActive(false);
+		howToPlayScreens[howToPlayScreenIndex].SetActive(true);
 	}
 	#endregion
 }
